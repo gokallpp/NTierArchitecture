c4a7254f8012426e9f980c0c4249f9c448215595
Abdullah GÖKALP <gokalppabdullah@outlook.com>
2026-09-22 23:19:34 +0300

    Add project files.

---
+++ BookClubApp.Entity/Entities/Book.cs
@@ -0,0 +1,13 @@
+﻿using BookClubApp.Entity.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClubApp.Entity.Entities
+{
+    public class Book : BaseEntity
+    {
+    }
+}
